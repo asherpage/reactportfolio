fecf1393bea2c4c1001a45e7ee98d9967189b213
asherpage <110576723+asherpage@users.noreply.github.com>
2024-04-11 15:17:09 -0700

    scrollin

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
 import React from 'react'
 import Navbar from'./component/Navbar.jsx'
 import './home.css'
-import me from './Image_20240328_115206_297-removebg-preview 1.png'
+import me from './new.PNG'
 
 const App = () => {
   return (
     <>
-    <div className='img'>
-      <Navbar />
-      <div className="main-content">
-        <div className="left-content">
-          {/* <h3 className='main-sub'>Hello, nice to meet you i am</h3> */}
-          <h1 className="big-title">Asher Page</h1>
-          <h3 className='main-info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae placeat consectetur adipisci, fugit perspiciatis accusantium.</h3>
-          <button className="custom-button">Button</button>
-        </div>
-        <div className="work-section">
-          <div className="work-boxes">
-            <div id='one' className="work-box"></div>
-            <div id='two' className="work-box"></div>
-            <div id='three' className="work-box"></div>
+    <Navbar />
+    <div className="container">
+      <img src={me} className="bottom-left-image" alt="Bottom Left Image" />
+      <div className="content">
+        {/* Text sections that will scroll on the right side */}
+        <div className="scrollable-text">
+          <div className="text-section">
+            <h2>Why Me</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum culpa fugiat natus facilis illum minus temporibus corporis exercitationem impedit, doloribus, dolores doloremque, numquam asperiores quasi magni? Est vitae dolor ex aliquam quas adipisci error recusandae delectus? Magnam nulla porro dicta, praesentium accusantium corrupti voluptatum illo sint maiores id iste sunt.</p>
+            <button>Contact Me</button>
           </div>
-          <h3>View All Work</h3>
+          <div className="text-section">
+            <h2>Latest work</h2>
+            <div className='proj-cont'>
+              <div className='boxer' id='first-box'></div>
+              <div className='boxer' id='second-box'></div>
+              <div className='boxer' id='third-box' ></div>
+              </div>
+          </div>
+          <div className="text-section">
+            <h2>Why Me</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum culpa fugiat natus facilis illum minus temporibus corporis exercitationem impedit, doloribus, dolores doloremque, numquam asperiores quasi magni? Est vitae dolor ex aliquam quas adipisci error recusandae delectus? Magnam nulla porro dicta, praesentium accusantium corrupti voluptatum illo sint maiores id iste sunt.</p>
+            <h2>Why Me</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum culpa fugiat natus facilis illum minus temporibus corporis exercitationem impedit, doloribus, dolores doloremque, numquam asperiores quasi magni? Est vitae dolor ex aliquam quas adipisci error recusandae delectus? Magnam nulla porro dicta, praesentium accusantium corrupti voluptatum illo sint maiores id iste sunt.</p>
+            <h2>Why Me</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum culpa fugiat natus facilis illum minus temporibus corporis exercitationem impedit, doloribus, dolores doloremque, numquam asperiores quasi magni? Est vitae dolor ex aliquam quas adipisci error recusandae delectus? Magnam nulla porro dicta, praesentium accusantium corrupti voluptatum illo sint maiores id iste sunt.</p>
+            <h2>Why Me</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum culpa fugiat natus facilis illum minus temporibus corporis exercitationem impedit, doloribus, dolores doloremque, numquam asperiores quasi magni? Est vitae dolor ex aliquam quas adipisci error recusandae delectus? Magnam nulla porro dicta, praesentium accusantium corrupti voluptatum illo sint maiores id iste sunt.</p>
+          </div>
+          
+          {/* Add more text sections as needed */}
         </div>
-      </div>
-      <div className="picture">
-        <img src={me} alt="Your Image" />
+        
       </div>
     </div>
+    
     </>
   )
 }
